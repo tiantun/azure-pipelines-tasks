d54ce3cd5092d9d57a3cd921912e4c6a940e6c91
Stan Zubov <stanzubov@microsoft.com>
2022-12-19 13:59:01 +0100

    2017337 Update Tasks - Common - G4 - JavaToolInstallerV0

---
+++ Tasks/JavaToolInstallerV0/build_task.sh
@@ -0,0 +1,1 @@
+node make.js build --task JavaToolInstallerV0 && tfx build tasks delete --task-id C0E0B74F-0931-47C7-AC27-7C5A19456A36 && tfx build tasks upload --task-path .\_build\Tasks\JavaToolInstallerV0
